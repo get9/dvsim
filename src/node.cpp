062da4499b5b9453c99fde5db3861720c2843fae
Sean Karlage <skarlage@get9.io>
2015-12-02 01:12:41 -0500

    Still fixing for gcc 4.6

--- src/node.cpp
+++ src/node.cpp
@@ -324,7 +324,7 @@ bool Node::update_dv_table(const DVMessage& msg)
 		if (dv_.find(node) == std::end(dv_)) {
 			// Distance will be dist to sender + distance provided by sender
 			DistAndNextHop dnh =
-				std::make_tuple(dist_to_sender + sender_to_node_dist, msg.sender);
+				DistAndNextHop(dist_to_sender + sender_to_node_dist, msg.sender);
 			dv_.emplace(node, std::move(dnh));
 			did_update_table = true;
 		}
